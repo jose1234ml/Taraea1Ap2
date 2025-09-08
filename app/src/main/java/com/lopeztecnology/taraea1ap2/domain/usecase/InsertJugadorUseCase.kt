a56e8bc71d22b5935066253eb4a8968879f39fac
malvin <malvi@DESKTOP-57EMM99>
2025-09-08 21:17:13 +0200

    crea caso de uso para obtener jugadores

---
+++ app/src/main/java/com/lopeztecnology/taraea1ap2/domain/usecase/InsertJugadorUseCase.kt
@@ -0,0 +1,12 @@
+package com.lopeztecnology.taraea1ap2.domain.usecase
+
+import com.lopeztecnology.taraea1ap2.data.local.Jugador
+import com.lopeztecnology.taraea1ap2.domain.repository.JugadorRepository
+
+class InsertJugadorUseCase(private val repository: JugadorRepository) {
+    suspend operator fun invoke(jugador: Jugador): Boolean {
+        // validación  campos obligatorios
+        if (jugador.nombres.isBlank() || jugador.partidas < 0) return false
+        return repository.insertJugador(jugador)
+    }
+}
