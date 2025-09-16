c117d2dca0d3d0ebefc318dfcaad86e77d161947
malvin <malvi@DESKTOP-57EMM99>
2025-09-17 00:49:47 +0200

    Define la entidad de la base de datos para Partida

---
+++ app/src/main/java/com/lopeztecnology/taraea1ap2/data/local/PartidaEntity.kt
@@ -0,0 +1,14 @@
+package com.lopeztecnology.taraea1ap2.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "partidas")
+data class PartidaEntity(
+    @PrimaryKey(autoGenerate = true) val partidaId: Int = 0,
+    val jugadorX: String,
+    val jugadorO: String,
+    val ganador: String? = null,
+    val fecha: Long = System.currentTimeMillis(),
+    val esFinalizada: Boolean = false
+)
