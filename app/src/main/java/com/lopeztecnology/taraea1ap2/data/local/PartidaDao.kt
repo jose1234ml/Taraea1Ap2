2453032d1a0c69373205010a8d3f207b43de75e7
malvin <malvi@DESKTOP-57EMM99>
2025-09-24 23:17:52 +0200

    Modifica PartidaDao.kt

--- app/src/main/java/com/lopeztecnology/taraea1ap2/data/local/PartidaDao.kt
+++ app/src/main/java/com/lopeztecnology/taraea1ap2/data/local/PartidaDao.kt
@@ -3,35 +3,24 @@ package com.lopeztecnology.taraea1ap2.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Query
 import androidx.room.Update
+import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PartidaDao {
-
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(partida: PartidaEntity): Long
+    suspend fun guardarPartida(partida: PartidaEntity): Long
 
     @Update
-    suspend fun update(partida: PartidaEntity)
+    suspend fun actualizarPartida(partida: PartidaEntity)
 
+    @Query("SELECT * FROM partidas WHERE jugadorX = :nombreJugador OR jugadorO = :nombreJugador ORDER BY fecha DESC LIMIT 1")
+    suspend fun obtenerUltimaPartida(nombreJugador: String): PartidaEntity?
 
     @Query("SELECT * FROM partidas WHERE jugadorX = :nombreJugador OR jugadorO = :nombreJugador ORDER BY fecha DESC")
-    fun getPartidasPorJugador(nombreJugador: String): Flow<List<PartidaEntity>>
-
+    fun obtenerPartidasPorJugador(nombreJugador: String): Flow<List<PartidaEntity>>
 
     @Query("SELECT * FROM partidas ORDER BY fecha DESC")
-    fun getTodasLasPartidas(): Flow<List<PartidaEntity>>
-
-
-    @Query("""
-        SELECT * FROM partidas 
-        WHERE (jugadorX = :nombreJugador OR jugadorO = :nombreJugador) 
-          AND esFinalizada = 0 
-        ORDER BY fecha DESC 
-        LIMIT 1
-    """)
-    suspend fun getUltimaPartidaIncompleta(nombreJugador: String): PartidaEntity?
+    fun obtenerTodasLasPartidas(): Flow<List<PartidaEntity>>
 }
