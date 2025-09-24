6c4a4e3ac118b6737a20e121b8621601d9587191
malvin <malvi@DESKTOP-57EMM99>
2025-09-24 23:16:57 +0200

    Modifica PartidaRepository.kt

--- app/src/main/java/com/lopeztecnology/taraea1ap2/domain/repository/PartidaRepository.kt
+++ app/src/main/java/com/lopeztecnology/taraea1ap2/domain/repository/PartidaRepository.kt
@@ -7,22 +7,22 @@ import kotlinx.coroutines.flow.Flow
 class PartidaRepository(private val dao: PartidaDao) {
 
     suspend fun guardarPartida(partida: PartidaEntity): Long {
-        return dao.insert(partida)
+        return dao.guardarPartida(partida)
     }
 
     suspend fun actualizarPartida(partida: PartidaEntity) {
-        dao.update(partida)
+        dao.actualizarPartida(partida)
     }
 
     fun obtenerPartidasPorJugador(nombreJugador: String): Flow<List<PartidaEntity>> {
-        return dao.getPartidasPorJugador(nombreJugador)
+        return dao.obtenerPartidasPorJugador(nombreJugador)
     }
 
     fun obtenerTodasLasPartidas(): Flow<List<PartidaEntity>> {
-        return dao.getTodasLasPartidas()
+        return dao.obtenerTodasLasPartidas()
     }
 
     suspend fun obtenerUltimaPartida(nombreJugador: String): PartidaEntity? {
-        return dao.getUltimaPartidaIncompleta(nombreJugador)
+        return dao.obtenerUltimaPartida(nombreJugador)
     }
 }
