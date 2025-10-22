326e8d20582cf64e04cf70d5365a2c2968b4c612
malvin <malvi@DESKTOP-57EMM99>
2025-10-23 00:17:02 +0200

    modfica MovimientosMapper.kt

--- app/src/main/java/com/lopeztecnology/taraea1ap2/tarea/mapper/MovimientosMapper.kt
+++ app/src/main/java/com/lopeztecnology/taraea1ap2/tarea/mapper/MovimientosMapper.kt
@@ -3,20 +3,18 @@ package com.lopeztecnology.taraea1ap2.tarea.mapper
 import com.lopeztecnology.taraea1ap2.data.dto.MovimientosDto
 import com.lopeztecnology.taraea1ap2.domain.model.Movimientos
 
-
-fun MovimientosDto.asDomain(): Movimientos = Movimientos(
-    movimientoId = this.movimientoId,
-    partidaId = this.partidaId,
-    jugador = this.jugador,
-    posicionFila = this.posicionFila,
-    posicionColumna = this.posicionColumna
+fun Movimientos.toDto(partidaId: Int): MovimientosDto = MovimientosDto(
+    movimientoId = movimientoId,
+    partidaId = partidaId,
+    jugador = jugador,
+    posicionFila = posicionFila,
+    posicionColumna = posicionColumna,
+)
+fun MovimientosDto.toMovimientos(): Movimientos = Movimientos(
+    movimientoId = movimientoId,
+    partidaId = partidaId,
+    jugador = jugador,
+    posicionFila = posicionFila,
+    posicionColumna = posicionColumna,
 )
 
-
-fun Movimientos.asDto(forPartidaId: Int): MovimientosDto = MovimientosDto(
-    movimientoId = this.movimientoId,
-    partidaId = forPartidaId,
-    jugador = this.jugador,
-    posicionFila = this.posicionFila,
-    posicionColumna = this.posicionColumna
-)
